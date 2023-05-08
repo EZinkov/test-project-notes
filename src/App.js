04baba799c2f05934f7233630c579d159356e925
Evgeniy <evgeniyzinkov98@gmail.com>
2023-05-08 16:35:41 +0200

    add: sidebar logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div className="App">notes App</div>
+import { useState } from "react"
+import "./App.css"
+import Sidebar from "./components/Sidebar"
+import Workspace from "./components/Workspace"
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+
+  const onAddNote = () => {
+    console.log("add")
+  }
+  return (
+    <div className="App">
+      <Sidebar notes={notes} onAddNote={onAddNote} />
+      <Workspace />
+    </div>
+  )
 }
 
 export default App
