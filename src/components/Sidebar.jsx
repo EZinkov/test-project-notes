62d2bfc20b60acdcdf698cc51329e2c8186196a4
Evgeniy <evgeniyzinkov98@gmail.com>
2023-05-08 16:35:49 +0200

    add: sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Sidebar = ({ notes, onAddNote }) => {
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button onClick={onAddNote}>Add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        {notes.map((note) => (
+          <div className="app-sidebar-note">
+            <div className="sidebar-note-title">
+              <strong>Title</strong>
+              <button>Delete</button>
+            </div>
+            <p>Note preview</p>
+            <small className="note-meta">Last modified [date+]</small>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
